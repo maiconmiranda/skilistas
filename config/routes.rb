3964abf75b9da70fe2363cd1758fe20da61f1b7d
maiconmiranda <cam022021@coderacademy.edu.au>
2020-11-17 17:35:58 +1100

    update routes to nest reviews and comments into profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+  
   devise_for :users
   resources :categories
-  resources :profiles
+  resources :profiles do 
+    resources :reviews, exept: [:show, :index]
+    resources :comments, exept: [:show, :index]
+  end
   resources :accounts
   
   root to: 'pages#home'

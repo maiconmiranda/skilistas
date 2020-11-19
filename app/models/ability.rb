edaa737e27ff2f93c82eb9d17514d3ac3ca8a580
maiconmiranda <cam022021@coderacademy.edu.au>
2020-11-19 16:21:15 +1100

    update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,17 +1,17 @@
-# frozen_string_literal: true
+frozen_string_literal: true
 
-# class Ability
-#   include CanCan::Ability
+class Ability
+  include CanCan::Ability
 
-#   def initialize(user)
-#     can :read, Profile, public: true
+  # def initialize(user)
+  #   can :read, Profile, public: true
     
-#     if user.admin?
-#       can :manage, :all
-#     if user.present?
-#       can :manage, Profile
-#       can :manage, Account, user_id: user.id
-#     end
-#     end
-#   end
-# end
+  #   if user.admin?
+  #     can :manage, :all
+  #   if user.present?
+  #     can :manage, Profile
+  #     can :manage, Account, user_id: user.id
+  #   end
+  #   end
+  # end
+end
